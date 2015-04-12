6ea681d3cf2c334c635482c901cc191f5f807383
kyousuke916 <goodkr@seed.net.tw>
2015-04-12 20:50:35 +0800

    場景 20% + 測試AI

---
+++ Assets/Script/AI/MonsterAI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterAI : MonoBehaviour
+{
+    [SerializeField]
+    protected float m_DetectInterval = 0.5f;
+
+    [SerializeField]
+    protected Transform m_TargetTs = null;
+
+    protected NavMeshAgent mAgent = null;
+
+    void Awake()
+    {
+        mAgent = GetComponent<NavMeshAgent>();
+
+        StartDetect();
+    }
+
+    private void StartDetect()
+    {
+        StartCoroutine(LoopDetect());
+    }
+
+    private IEnumerator LoopDetect()
+    {
+        while (true)
+        {
+            Follow();
+
+            yield return new WaitForSeconds(m_DetectInterval);
+        }
+    }
+
+    private void Follow()
+    {
+        mAgent.SetDestination(m_TargetTs.position);
+    }
+}
