dc8af0ebbaf874de7487d2cb466cf20f67feeb10
septem1120 <septem.huang@gamesofa.com>
2015-04-14 09:40:29 +0800

    Menu初始化
    init

---
+++ Menu/Assets/button.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class button : MonoBehaviour {
+
+	private void OnGUI()
+	{
+		if(GUI.Button(new Rect(15, 15, 200, 100), "Load Level"))
+			Application.LoadLevel("GameScene");
+	}
+}
