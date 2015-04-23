734a7d9ac5314278cd354c177324d133972cca61
kyousuke916 <goodkr@seed.net.tw>
2015-04-24 01:01:22 +0800

    ¥[¤J¯¥¶ðº޲z¡A¹CÀ¸º޲zÃþ§O µ¥...

--- Assets/Script/CannonPlatform/CannonPlatform.cs
+++ Assets/Script/CannonPlatform/CannonPlatform.cs
@@ -4,39 +4,61 @@ using System.Collections;
 /// <summary>砲台管理類別</summary>
 public class CannonPlatform : MonoBehaviour
 {
+    private static readonly Vector3 OFFSET = new Vector3(0f, 0.5f, 0f);
     private ICannon mCannon = null;
+
+    private Transform mTs = null;
+
+    public Vector3 Position { get { return mTs.position + OFFSET; } }
 
     public bool IsEmpty { get { return mCannon == null; } }
     public bool HasCannon { get { return !IsEmpty; } }
 
     public string ID { get { return name; } }
 
-    public bool PutCannon(ICannon cannon)
+    void Awake()
+    {
+        mTs = transform;
+    }
+
+    /// <summary>建置砲塔</summary>
+    public void BuildCannon(ICannon cannon)
     {
         if (HasCannon)
         {
             Debug.LogErrorFormat("位置:{0} 已放置砲塔:{1}", name, ID);
-            return false;
+            return;
         }
 
-        mCannon = cannon;
-
-        return true;
+        CreateCannon(cannon);
     }
 
-    public ICannon RemoveCannon()
+    /// <summary>賣掉砲塔</summary>
+    public void SellCannon()
     {
         if (IsEmpty)
         {
             Debug.LogErrorFormat("位置:{0} 沒有砲塔可移除", name);
-            return null;
+            return;
         }
 
-        var cannon = mCannon;
+        RemoveCannon();
+    }
+
+    private void CreateCannon(ICannon cannon)
+    {
+        //call cost function...
+
+        int cost = cannon.Cost;
+    }
+
+    private void RemoveCannon()
+    {
+        //call sell function...
+
+        int price = mCannon.Price;
 
         mCannon = null;
-
-        return cannon;
     }
 
 }
