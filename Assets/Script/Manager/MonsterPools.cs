71fa93284c3e7bd9a0bd66e0035dbd1b2b56b36c
kyousuke916 <goodkr@seed.net.tw>
2015-04-28 01:45:29 +0800

    1.¥[¤J­µ¼Ö 2.¹CÀ¸¤º¥[¤J¥ú·Ó(Åܺ}«G¤F³á~) 3.©Ǫ«¦º¤`¦^¦¬«ÍÅ魫½ƧQ¥Î

---
+++ Assets/Script/Manager/MonsterPools.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MonsterPools : MonoSingleTon<MonsterPools>
+{
+    [SerializeField]
+    private Transform m_MonsterContainer = null;
+    
+    private Dictionary<string, Queue<MonsterAI>> mMonsterDict = new Dictionary<string, Queue<MonsterAI>>();
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+    }
+
+    public static void Retrieve(MonsterAI monsterAI)
+    {
+        Instance.InsRetrieve(monsterAI);
+    }
+
+    public void InsRetrieve(MonsterAI monsterAI)
+    {
+        string id = monsterAI.MonsterID;
+
+        if (!mMonsterDict.ContainsKey(id))
+            mMonsterDict[id] = new Queue<MonsterAI>();
+
+        monsterAI.SetDisable();
+
+        var queue = mMonsterDict[id];
+        queue.Enqueue(monsterAI);
+    }
+
+    public MonsterAI Obtain(string monsterID)
+    {
+        Queue<MonsterAI> queue;
+
+        if (mMonsterDict.TryGetValue(monsterID, out queue))
+        {
+            if (queue.Count > 0)
+                return queue.Dequeue();
+        }
+
+        return CreateMonster(monsterID);
+    }
+
+    public MonsterAI CreateMonster(string monsterID)
+    {
+        var path = string.Format("Monster/{0}", monsterID);
+        var prefab = Resources.Load<GameObject>(path);
+        var enemyGo = Instantiate(prefab) as GameObject;
+        var enemyTs = enemyGo.transform;
+        enemyTs.SetParent(m_MonsterContainer);
+
+        var monsterAI = enemyTs.GetComponent<MonsterAI>();
+
+        return monsterAI;
+    }
+}
