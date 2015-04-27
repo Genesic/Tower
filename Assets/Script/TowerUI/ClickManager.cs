b295626c040cd4e38c19f75dec5631ffa2777362
Genesic <genesicx@gmail.com>
2015-04-28 01:03:19 +0800

    你家UI超級大爆炸

---
+++ Assets/Script/TowerUI/ClickManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickManager : MonoBehaviour {
+
+	public GameObject selectTowerPannel;
+	
+	void Update () {
+	/*
+		if (Input.GetMouseButtonDown (1)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if( Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("CannonPlatform") ) ){
+				CannonPlatform selectCannon = hit.transform.gameObject.GetComponent<CannonPlatform>();
+				Debug.Log (selectCannon);
+				selectTowerPannel.GetComponent<SelectTower>().setCannon(selectCannon);
+			}
+		}
+*/
+		if (Input.GetMouseButtonDown (0) ) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if( Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("CannonPlatform") ) ){
+				openTowerPanel();
+			}
+		}
+	}
+
+	void openTowerPanel (){
+		Vector3 newPosition = Input.mousePosition;
+		newPosition.x += 64.0F;
+		newPosition.y -= 110.0F;
+		
+		if( newPosition.x > 776 ){
+			newPosition.x -= 128.0F;
+		}
+		if( newPosition.y < 120 ){
+			newPosition.y += 220.0F;
+		}
+		selectTowerPannel.transform.position = newPosition;
+		Debug.Log ( "origin:" + Input.mousePosition + " new: " + newPosition);
+		selectTowerPannel.SetActive(true);
+	}
+}
---
+++ Assets/Script/TowerUI/SelectTower.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SelectTower : MonoBehaviour {
+	[SerializeField]
+	private GameObject Panel;
+	public Tower useTower;
+	private CannonPlatform useCannon;
+
+	public void setCannon(CannonPlatform selectCannon)
+	{
+		useCannon = selectCannon;
+	}
+
+	public void buildTower()
+	{
+		if (useCannon != null) {
+			Vector3 TowerPosition = useCannon.Position;
+			Quaternion TowerRotation = Quaternion.identity;
+			Instantiate (useTower, TowerPosition, TowerRotation);
+		} else {
+			Debug.Log (useCannon);
+		}
+		Panel.SetActive(false);
+	}
+
+	public void testMessage(){
+		Debug.Log ("hello~" + useTower );
+	}
+}
