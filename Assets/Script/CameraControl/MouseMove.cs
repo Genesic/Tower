bd7d4bdd403aec7972bd2397eca20b2c990415b7
septem1120 <septem.huang@gamesofa.com>
2015-05-10 15:19:34 +0800

    增加鍵盤控制Camera

--- Assets/Script/CameraControl/MouseMove.cs
+++ Assets/Script/CameraControl/MouseMove.cs
@@ -6,6 +6,9 @@ public class MouseMove : MonoBehaviour {
 	public float turnSpeed = 4.0f;		// Speed of camera turning when mouse moves in along an axis
 	public float panSpeed = 4.0f;		// Speed of the camera when being panned
 	public float zoomSpeed = 4.0f;		// Speed of the camera going back and forth
+
+	public float keyboardMoveSpeed = 0.5f;
+	public float keyboardTurnSpeed = 2.0f;
 	
 	private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
 	private bool isPanning;			// Is the camera being panned?
@@ -14,12 +17,15 @@ public class MouseMove : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		turnSpeed = 4.0f;		// Speed of camera turning when mouse moves in along an axis
+		panSpeed = 4.0f;		// Speed of the camera when being panned
+		zoomSpeed = 4.0f;		// Speed of the camera going back and forth
+		keyboardMoveSpeed = 0.5f;
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		// --- mouse part ---
 		// Get the left mouse button
 		if(Input.GetMouseButtonDown(0))
 		{
@@ -36,27 +42,16 @@ public class MouseMove : MonoBehaviour {
 			isRotating = true;
 		}
 		
-		// Get the middle mouse button
-//		if(Input.GetAxis("Mouse ScrollWheel") > 0 || Input.GetAxis("Mouse ScrollWheel") < 0)
-//		{
-			// Get mouse origin
-//			mouseOrigin = Input.mousePosition;
-//			isZooming = true;
-//		}
-		
 		// Disable movements on button release
 		if (!Input.GetMouseButton(0)) 
 			isPanning=false;
 		if (!Input.GetMouseButton(1)) 
 			isRotating=false;
-//		if (!(Input.GetAxis("Mouse ScrollWheel") > 0) || !(Input.GetAxis("Mouse ScrollWheel") < 0)) 
-//			isZooming=false;
 		
 		// Move the camera on it's XY plane
 		if (isPanning)
 		{
 			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-
 			Vector3 move = new Vector3(- pos.x * panSpeed, 0, - pos.y * panSpeed);
 			transform.Translate(move);
 		}
@@ -65,9 +60,53 @@ public class MouseMove : MonoBehaviour {
 		if (isRotating)
 		{
 			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-
-//			transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
 			transform.RotateAround(transform.position, Vector3.up, -pos.x * turnSpeed);
+		}
+
+		// 	--- keyboard part ---
+		//	--move--
+		if (Input.GetKey(KeyCode.W))
+		{
+			Vector3 move = new Vector3(0, 0, keyboardMoveSpeed);
+			transform.Translate(move);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			Vector3 move = new Vector3(0, 0, -keyboardMoveSpeed);
+			transform.Translate(move);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			Vector3 move = new Vector3(-keyboardMoveSpeed, 0, 0);
+			transform.Translate(move);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			Vector3 move = new Vector3(keyboardMoveSpeed, 0, 0);
+			transform.Translate(move);
+		}
+
+		//	--Rotate--
+		if (Input.GetKey(KeyCode.Q))
+		{
+
+			transform.RotateAround(transform.position, new Vector3(0, -1, 0), keyboardTurnSpeed);
+		}
+		if (Input.GetKey(KeyCode.E))
+		{
+			transform.RotateAround(transform.position, new Vector3(0, 1, 0), keyboardTurnSpeed);
+		}
+		if (Input.GetKey(KeyCode.Alpha2))
+		{
+			GameObject cam = GameObject.Find("Main Camera");
+			if(cam)
+				cam.transform.Rotate(-1, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.X))
+		{
+			GameObject cam = GameObject.Find("Main Camera");
+			if(cam)
+				cam.transform.Rotate(1, 0, 0);
 		}
 	}
 
