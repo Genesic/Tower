beec71b59e92cbd440fc44ca80f9939a0c83e92c
septem1120 <septem.huang@gamesofa.com>
2015-05-27 01:39:39 +0800

    補Script

--- Assets/Scene/menu/Script/ButtonClick.cs
+++ Assets/Scene/menu/Script/ButtonClick.cs
@@ -3,24 +3,105 @@ using System.Collections;
 
 public class ButtonClick : MonoBehaviour 
 {
-	public void StartClick(string levelName)
+	public void LevelClick(int num)
 	{
-		Application.LoadLevel (levelName);
+		Application.LoadLevel ("game");
 	}
 
-	public void SettingClick(string levelName)
+	public void StartClick()
 	{
-		Application.LoadLevel (levelName);
+		Animator anim;
+		GameObject menu = GameObject.Find ("MenuBg");
+		anim = menu.GetComponent<Animator> ();
+		anim.CrossFade ("MainMenuOut", 0f);
+		
+		Animator anim2;
+		GameObject menu2 = GameObject.Find ("LevelSelect");
+		anim2 = menu2.GetComponent<Animator> ();
+		anim2.CrossFade ("SettingMenuInto", 0.5f);
 	}
 
-	public void CreditClick(string levelName)
+	public void SettingClick()
 	{
-		Application.LoadLevel (levelName);
+		Animator anim;
+		GameObject menu = GameObject.Find ("MenuBg");
+		anim = menu.GetComponent<Animator> ();
+		anim.CrossFade ("MainMenuOut", 0f);
+
+		Animator anim2;
+		GameObject menu2 = GameObject.Find ("Setting");
+		anim2 = menu2.GetComponent<Animator> ();
+		anim2.CrossFade ("SettingMenuInto", 0.5f);
+	}
+
+	public void MusicToggle()
+	{
+		AudioSource audioSource;
+		GameObject obj = GameObject.Find ("EventSystem");
+		audioSource = obj.GetComponent<AudioSource> ();
+		audioSource.mute = !audioSource.mute;
+	}
+
+	public void CreditClick()
+	{
+		Animator anim;
+		GameObject menu = GameObject.Find ("MenuBg");
+		anim = menu.GetComponent<Animator> ();
+		anim.CrossFade ("MainMenuOut", 0f);
+		
+		Animator anim2;
+		GameObject menu2 = GameObject.Find ("Shop");
+		anim2 = menu2.GetComponent<Animator> ();
+		anim2.CrossFade ("SettingMenuInto", 0.5f);
+		
 	}
 
 	public void ExitClick(string levelName)
 	{
-		Application.Quit ();
+		Application.Quit();
 	}
+
+
+	public void LevelBackClick()
+	{
+		Animator anim;
+		GameObject menu = GameObject.Find ("MenuBg");
+		anim = menu.GetComponent<Animator> ();
+		anim.CrossFade ("MainMenuInto", 0.5f);
+		
+		Animator anim2;
+		GameObject menu2 = GameObject.Find ("LevelSelect");
+		anim2 = menu2.GetComponent<Animator> ();
+		anim2.CrossFade ("SettingMenuOut", 0f);
+	}
+
+	public void SettingBackClick()
+	{
+		Animator anim;
+		GameObject menu = GameObject.Find ("MenuBg");
+		anim = menu.GetComponent<Animator> ();
+		anim.CrossFade ("MainMenuInto", 0.5f);
+
+		Animator anim2;
+		GameObject menu2 = GameObject.Find ("Setting");
+		anim2 = menu2.GetComponent<Animator> ();
+		anim2.CrossFade ("SettingMenuOut", 0f);
+	}
+
+	public void ShopBackClick()
+	{
+		Animator anim;
+		GameObject menu = GameObject.Find ("MenuBg");
+		anim = menu.GetComponent<Animator> ();
+		anim.CrossFade ("MainMenuInto", 0.5f);
+		
+		Animator anim2;
+		GameObject menu2 = GameObject.Find ("Shop");
+		anim2 = menu2.GetComponent<Animator> ();
+		anim2.CrossFade ("SettingMenuOut", 0f);
+	}
+
+
+
 
 }
